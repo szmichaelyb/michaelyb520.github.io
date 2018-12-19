3313e4d477922743af2b6deb886389356d388b39
Michaelyb520 <480392923@qq.com>
2018-12-19 19:37:08 +0800

    Update - 更新404页面设置

--- components/jquery/src/manipulation/_evalUrl.js
+++ components/jquery/src/manipulation/_evalUrl.js
@@ -2,6 +2,8 @@ define( [
 	"../ajax"
 ], function( jQuery ) {
 
+"use strict";
+
 jQuery._evalUrl = function( url ) {
 	return jQuery.ajax( {
 		url: url,
@@ -9,9 +11,19 @@ jQuery._evalUrl = function( url ) {
 		// Make this explicit, since user can override this through ajaxSetup (#11264)
 		type: "GET",
 		dataType: "script",
+		cache: true,
 		async: false,
 		global: false,
-		"throws": true
+
+		// Only evaluate the response if it is successful (gh-4126)
+		// dataFilter is not invoked for failure responses, so using it instead
+		// of the default converter is kludgy but it works.
+		converters: {
+			"text script": function() {}
+		},
+		dataFilter: function( response ) {
+			jQuery.globalEval( response );
+		}
 	} );
 };
 
