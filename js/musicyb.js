8df803ecba092db33d451f9514edf4d777325f4a
Michaelyb520 <480392923@qq.com>
2019-05-11 17:33:50 +0800

    Update - 删除一些不需要的笔记

---
+++ js/musicyb.js
@@ -0,0 +1,7 @@
+
+YBmuiscPlay({
+    el: "musicapp",
+    audioUrl: "{{'/assets/mp3/大王叫我来巡山.mp3' | prepend: site.baseurl }}",
+    position: "top:3px;right:3px",//右上角
+    animaClass: "musicIconRotate"
+});
